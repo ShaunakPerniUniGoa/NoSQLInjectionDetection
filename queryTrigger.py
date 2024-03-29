1b8a40124f220b90e6e20c64906bc4d3324894ee
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2024-03-29 16:20:56 +0530

    commit

--- queryTrigger.py
+++ queryTrigger.py
@@ -1,2 +1,11 @@
-import nbformat
+import json
+import pymongo
+import os
 
+print(os.path)
+print(os.listdir())
+
+with open("No-SQL_Gen/No-SqlDataset.json", "r") as queryFile:
+    data = json.load(queryFile)
+
+print(queryFile)
